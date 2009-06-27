9c427d4134defb9698f78f99f1d1b32cf5dede89
Mark Grimes <mgrimes@cpan.org>
2009-06-27 16:59:52 +0000

    Import of MGRIMES/App-EditorTools-0.02 from CPAN.
    gitpan-cpan-distribution: App-EditorTools
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MGRIMES/App-EditorTools-0.02.tar.gz
    gitpan-cpan-author:       MGRIMES
    gitpan-cpan-maturity:     released

---
+++ lib/App/EditorTools/Command/RenamePackageFromPath.pm
@@ -0,0 +1,28 @@
+package App::EditorTools::Command::RenamePackageFromPath;
+
+use strict;
+use warnings;
+
+use App::EditorTools -command;
+
+sub opt_spec {
+    return ( [ "filename|f=s", "The filename and path of the package", ] );
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+    $self->usage_error("Filename is required") unless $opt->{filename};
+}
+
+sub run {
+    my ( $self, $opt, $arg ) = @_;
+
+    my $doc_as_str = join( "", <STDIN> );
+
+    require PPIx::EditorTools::RenamePackageFromPath;
+    print PPIx::EditorTools::RenamePackageFromPath->new->rename(
+        code     => $doc_as_str,
+        filename => $opt->{filename} )->code;
+}
+
+1;
